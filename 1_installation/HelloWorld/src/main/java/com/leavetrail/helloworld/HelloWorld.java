92cb1994bfbd095ecab1052f621d95ed69654340
user <user@localhost>
2021-09-19 23:44:07 +0200

    Adding some changes to feature_1

--- 1_installation/HelloWorld/src/main/java/com/leavetrail/helloworld/HelloWorld.java
+++ 1_installation/HelloWorld/src/main/java/com/leavetrail/helloworld/HelloWorld.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * Adding some changes to feature_1 branch.
  */
 package com.leavetrail.helloworld;
 
